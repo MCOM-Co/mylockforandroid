87bb1582e64b0d756004842e3ef0e78baac83b88
myLockandroid <myLockandroid@d2e66b82-e105-11de-81d3-db62269da9c1>
2010-01-16 18:10:41 +0000

    Functional to add one widget. Need to implement a subclass of viewgroup which would handle being the parent of all child AppWidgetHostView objects that will be created when the user adds widgets. When this code adds a widget it passes that view itself as the parent so it gets drawn in the center.

--- custom lock/src/i4nc4mp/customLock/Lockscreen.java
+++ custom lock/src/i4nc4mp/customLock/Lockscreen.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 
 import android.app.Activity;
 import android.appwidget.AppWidgetHost;
+import android.appwidget.AppWidgetHostView;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProviderInfo;
 import android.content.ComponentName;
@@ -24,12 +25,18 @@ public class Lockscreen extends Activity {
 private AppWidgetManager mAppWidgetManager;
 private AppWidgetHost mAppWidgetHost;
 
-private LayoutInflater mInflater;
+//private LayoutInflater mInflater;
 
 static final int APPWIDGET_HOST_ID = 2037;
+//this int identifies you. The Launcher's ID is 1024.
+//If you were to implement different hosts that need to be distinguished
+//then the ID is a shortcut for passing what you're doing to the correct one 
+
 
 private static final int REQUEST_CREATE_APPWIDGET = 5;
 private static final int REQUEST_PICK_APPWIDGET = 9;
+
+private AppWidgetHostView widgetview;
 
 @Override
 protected void onCreate(Bundle savedInstanceState) {
@@ -39,7 +46,7 @@ protected void onCreate(Bundle savedInstanceState) {
     requestWindowFeature(android.view.Window.FEATURE_NO_TITLE);
     updateLayout();
     
-    mInflater = getLayoutInflater();
+    //mInflater = getLayoutInflater();
     
     mAppWidgetManager = AppWidgetManager.getInstance(this);
 
@@ -53,12 +60,16 @@ private void updateLayout() {
     LayoutInflater inflater = LayoutInflater.from(this);
 
     setContentView(inflateView(inflater));
+    //if (mInflater != null) mInflater = inflater;
 }
 
 //override is for when we subclass LockActivity for purpose pass the blank slate layout
 //@Override
 protected View inflateView(LayoutInflater inflater) {
-    return inflater.inflate(R.layout.mylockscreen, null);
+    if(widgetview==null) return inflater.inflate(R.layout.mylockscreen, null);
+    else return inflater.inflate(R.layout.mylockscreen, widgetview);
+    //tries to pass the widget host view
+    //what this will do once implemented is pass the view that is parent to all widget children
 }
 
 public boolean onCreateOptionsMenu(Menu menu) {
@@ -76,7 +87,12 @@ public boolean onOptionsItemSelected(MenuItem item) {
     return false;
 }
 
-//user presses menu, then selects Add Widget from the popup menu
+//here's where it gets really fun. we're going to launch the widget pick-list intent 
+
+//there is a bug with this intent/process where they have made it dependent on defining a custom extra widget. 
+//The "search" widget is not coded as an AppWidgetProvider for some reason 
+//they insert it into the list when pick intent is called, and if you don't insert a custom item 
+//you get a null pointer exception when trying to start 
 protected void doWidgetPick() {
 	int appWidgetId = Lockscreen.this.mAppWidgetHost.allocateAppWidgetId();
 
@@ -84,12 +100,13 @@ protected void doWidgetPick() {
     pickIntent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, appWidgetId);
     
     /* custom extra that has to be there or else NPE will happen due to android bug   */
+  //this is pulled from the Launcher source, I just changed a few things as it's just a dummy entry at this point 
     ArrayList<AppWidgetProviderInfo> customInfo =
             new ArrayList<AppWidgetProviderInfo>();
     AppWidgetProviderInfo info = new AppWidgetProviderInfo();
     info.provider = new ComponentName(getPackageName(), "XXX.YYY");
-    info.label = "i love android";//getString(R.string.group_search);
-    info.icon = R.drawable.icon;//R.drawable.ic_search_widget;
+    info.label = "i love android";
+    info.icon = R.drawable.icon;
     customInfo.add(info);
     pickIntent.putParcelableArrayListExtra(
             AppWidgetManager.EXTRA_CUSTOM_INFO, customInfo);
@@ -100,28 +117,30 @@ protected void doWidgetPick() {
     pickIntent.putParcelableArrayListExtra(
             AppWidgetManager.EXTRA_CUSTOM_EXTRAS, customExtras);
     /* that's a lot of lines that are there for no function at all */
-    
-    
-    
+        
     
     // start the pick activity
     startActivityForResult(pickIntent, REQUEST_PICK_APPWIDGET);
-    //what's happening here is that result comes back immediately as 9, 0, null
-    //logcat says that activity is starting as a new task so canceling result
+    //because we've defined ourselves as a singleTask activity, it will allow this intent to be part of the task
+
 }
 
 
 @Override
 protected void onActivityResult(int requestCode, int resultCode, Intent data) {
     Log.v("result",requestCode + ", " + resultCode + ", " + data);
-    //Canceled (0) is coming back immediately due to the "Starting as a new task" log
+  //when the user completes the pick process, both these cases come back, according to log. 
+  //looks like this is happening because Pick does a check to see if a config needs to be launched first 
+  //if not it just sends the create intent 
+    
+    
     if (resultCode == RESULT_OK) {
         switch (requestCode) {
         	case REQUEST_PICK_APPWIDGET:
         		addAppWidget(data);
         		break;
         	case REQUEST_CREATE_APPWIDGET:
-                //completeAddAppWidget(data, mAddItemCellInfo, !mDesktopLocked);
+                completeAddAppWidget(data);
                 break;
         }
     }
@@ -134,143 +153,57 @@ protected void onActivityResult(int requestCode, int resultCode, Intent data) {
             mAppWidgetHost.deleteAppWidgetId(appWidgetId);
         }
     }
-    
-	/*
-	mWaitingForResult = false;
-
-    // The pattern used here is that a user PICKs a specific application,
-    // which, depending on the target, might need to CREATE the actual target.
-
-    // For example, the user would PICK_SHORTCUT for "Music playlist", and we
-    // launch over to the Music app to actually CREATE_SHORTCUT.
-
-    if (resultCode == RESULT_OK && mAddItemCellInfo != null) {
-        switch (requestCode) {
-            case REQUEST_PICK_APPLICATION:
-                completeAddApplication(this, data, mAddItemCellInfo, !mDesktopLocked);
-                break;
-            case REQUEST_PICK_SHORTCUT:
-                processShortcut(data, REQUEST_PICK_APPLICATION, REQUEST_CREATE_SHORTCUT);
-                break;
-            case REQUEST_CREATE_SHORTCUT:
-                completeAddShortcut(data, mAddItemCellInfo, !mDesktopLocked);
-                break;
-            case REQUEST_PICK_LIVE_FOLDER:
-                addLiveFolder(data);
-                break;
-            case REQUEST_CREATE_LIVE_FOLDER:
-                completeAddLiveFolder(data, mAddItemCellInfo, !mDesktopLocked);
-                break;
-            case REQUEST_PICK_APPWIDGET:
-                addAppWidget(data);
-                break;
-            case REQUEST_CREATE_APPWIDGET:
-                completeAddAppWidget(data, mAddItemCellInfo, !mDesktopLocked);
-                break;
-        }
-    } else if ((requestCode == REQUEST_PICK_APPWIDGET ||
-            requestCode == REQUEST_CREATE_APPWIDGET) && resultCode == RESULT_CANCELED &&
-            data != null) {
-        // Clean up the appWidgetId if we canceled
-        int appWidgetId = data.getIntExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, -1);
-        if (appWidgetId != -1) {
-            mAppWidgetHost.deleteAppWidgetId(appWidgetId);
-        }
-    }*/
 }
 
 
-/*
- //Add a widget to the workspace.
- 
- //@param data The intent describing the appWidgetId.
- //@param cellInfo The position on screen where to create the widget.
-
-//this segment appears to be needed as a result of users long press anywhere
-//that the custom subclass is catching
-
-
-private void completeAddAppWidget(Intent data, //CellLayout.CellInfo cellInfo,
-        boolean insertAtFirst) {
+private void completeAddAppWidget(Intent data){
+	//actually creates the view for the widget
 
     Bundle extras = data.getExtras();
     int appWidgetId = extras.getInt(AppWidgetManager.EXTRA_APPWIDGET_ID, -1);
 
-    //if (LOGD) d(LOG_TAG, "dumping extras content="+extras.toString());
-
     AppWidgetProviderInfo appWidgetInfo = mAppWidgetManager.getAppWidgetInfo(appWidgetId);
 
-    // Calculate the grid spans needed to fit this widget
-    //CellLayout layout = (CellLayout) mWorkspace.getChildAt(cellInfo.screen);
-    //int[] spans = layout.rectToCell(appWidgetInfo.minWidth, appWidgetInfo.minHeight);
-
-
+    /* Launcher would calculate the grid spans needed to fit this widget
+     * It would also do a check operation to abort if the cell user picked wasn't acceptable
+     * given the size of the widget they chose
+     */
     
-    // Try finding open space on Launcher screen
-    final int[] xy = mCellCoordinates;
-    if (!findSlot(cellInfo, xy, spans[0], spans[1])) {
-        if (appWidgetId != -1) mAppWidgetHost.deleteAppWidgetId(appWidgetId);
-        return;
-    }
+    //What we'll do is log the info about the widget for help in letting user reposition it
+    
+    int widgetwidth = appWidgetInfo.minWidth;
+    int widgetheight = appWidgetInfo.minHeight;
+    
+/*
+next we need to make a record of where we are adding this widget
+what the launcher is doing is spawning a helper object where it saves details about the widget
+it saves the number of cells wide and tall the widget is
+it adds the spawned object to the array list for widgetinfos
+the array list is a member of LauncherInfo helper object
+the model seems to retain the references to everything that's been placed on the Launcher
+*/
 
-    //Build Launcher-specific widget info and save to database
-    LauncherAppWidgetInfo launcherInfo = new LauncherAppWidgetInfo(appWidgetId);
-    launcherInfo.spanX = spans[0];
-    launcherInfo.spanY = spans[1];
 
-    LauncherModel.addItemToDatabase(this, launcherInfo,
-            LauncherSettings.Favorites.CONTAINER_DESKTOP,
-            mWorkspace.getCurrentScreen(), xy[0], xy[1], false);
-
-    if (!mRestoring) {
-        sModel.addDesktopAppWidget(launcherInfo);
-
-        //Perform actual inflation because we're live
-        launcherInfo.hostView = mAppWidgetHost.createView(this, appWidgetId, appWidgetInfo);
-
-        launcherInfo.hostView.setAppWidget(appWidgetId, appWidgetInfo);
-        launcherInfo.hostView.setTag(launcherInfo);
-
-        mWorkspace.addInCurrentScreen(launcherInfo.hostView, xy[0], xy[1],
-                launcherInfo.spanX, launcherInfo.spanY, insertAtFirst);
-    } else if (sModel.isDesktopLoaded()) {
-        sModel.addDesktopAppWidget(launcherInfo);
-    }
-}*/
+        widgetview = mAppWidgetHost.createView(this, appWidgetId, appWidgetInfo);
+        //launcher is doing something to pass this view to their the workspace or the celllayout
+        //I have to learn how to maintain a framelayout so i can attach the widgets to it
+        //it seems in the Launcher a workspace contains all the screens and the CellLayout is one screen
+        
+        updateLayout();
+        //the inflate method will see if widgetview is null - when not null it passes the widgetview as the parent.
+        //that operation creates the widget you chose in the center of the screen
+        //and the widget works.
+        
+        //to get it working the inflate method would pass a parent that we interact with to place each widgets
+        //so every single widget that gets created is one instance of the AppWidgetHostView.
+        //the viewgroup we would have to maintain holds all the appwidgethostviews/
+}
 
 public AppWidgetHost getAppWidgetHost() {
     return mAppWidgetHost;
 }
 
 
-
-/* this gets called in the onclick for the add to home dialog, to invoke the widget picking thing
-  case AddAdapter.ITEM_APPWIDGET: {
-                    int appWidgetId = Launcher.this.mAppWidgetHost.allocateAppWidgetId();
-
-                    Intent pickIntent = new Intent(AppWidgetManager.ACTION_APPWIDGET_PICK);
-                    pickIntent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, appWidgetId);
-                    // add the search widget
-                    ArrayList<AppWidgetProviderInfo> customInfo =
-                            new ArrayList<AppWidgetProviderInfo>();
-                    AppWidgetProviderInfo info = new AppWidgetProviderInfo();
-                    info.provider = new ComponentName(getPackageName(), "XXX.YYY");
-                    info.label = getString(R.string.group_search);
-                    info.icon = R.drawable.ic_search_widget;
-                    customInfo.add(info);
-                    pickIntent.putParcelableArrayListExtra(
-                            AppWidgetManager.EXTRA_CUSTOM_INFO, customInfo);
-                    ArrayList<Bundle> customExtras = new ArrayList<Bundle>();
-                    Bundle b = new Bundle();
-                    b.putString(EXTRA_CUSTOM_WIDGET, SEARCH_WIDGET);
-                    customExtras.add(b);
-                    pickIntent.putParcelableArrayListExtra(
-                            AppWidgetManager.EXTRA_CUSTOM_EXTRAS, customExtras);
-                    // start the pick activity
-                    startActivityForResult(pickIntent, REQUEST_PICK_APPWIDGET);
-                    break;
-                }
- */
 
 
 void addAppWidget(Intent data) {
