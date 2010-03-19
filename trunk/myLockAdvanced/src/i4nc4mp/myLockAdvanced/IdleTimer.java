bd3200f717fdfc6bb0c1286c662a0386d82a71e4
myLockandroid <myLockandroid@d2e66b82-e105-11de-81d3-db62269da9c1>
2010-03-19 04:56:23 +0000

    first import

---
+++ trunk/myLockAdvanced/src/i4nc4mp/myLockAdvanced/IdleTimer.java
@@ -0,0 +1,34 @@
+package i4nc4mp.myLockAdvanced;
+
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+
+public class IdleTimer {
+    private static final int REQUEST_ID = 0;
+    private static final long DEFAULT_TIMEOUT = 1 * 60 * 1000;  // 1 minute for testing
+
+    //intent will be caught in both the mediator and the lockactivity
+    private static PendingIntent buildIntent(Context ctx) {
+        Intent intent = new Intent("i4nc4mp.myLockAdvanced.lifecycle.IDLE_TIMEOUT");
+        PendingIntent sender = PendingIntent.getBroadcast(ctx, REQUEST_ID, intent, PendingIntent.FLAG_CANCEL_CURRENT);
+
+        return sender;
+    }
+
+    public static void start(Context ctx) {
+        long triggerTime = System.currentTimeMillis() + DEFAULT_TIMEOUT;
+
+        AlarmManager am = (AlarmManager) ctx.getSystemService(Context.ALARM_SERVICE);
+
+        am.set(AlarmManager.RTC, triggerTime, buildIntent(ctx));
+    }
+
+    public static void cancel(Context ctx) {
+        AlarmManager am = (AlarmManager) ctx.getSystemService(Context.ALARM_SERVICE);
+
+        am.cancel(buildIntent(ctx));
+    }
+
+}
