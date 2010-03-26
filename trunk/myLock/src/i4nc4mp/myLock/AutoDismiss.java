c2760200de7fd723e6a838223ae0642a732cc0bb
myLockandroid <myLockandroid@d2e66b82-e105-11de-81d3-db62269da9c1>
2010-03-26 04:53:14 +0000

    re-enabled shake code within the non sliderguarded mediator (AutoDismiss) and added checkbox that correctly enables the setting in the activity. we need to fix how the shake code is running, it seems to work ok several times then totally stop, but starts working again when i plug in for live usb logging, then quits as soon as i unplug (and it is really hard to shake it while plugged into the debug cable... lol)

--- trunk/myLock/src/i4nc4mp/myLock/AutoDismiss.java
+++ trunk/myLock/src/i4nc4mp/myLock/AutoDismiss.java
@@ -7,6 +7,10 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.SharedPreferences;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.PowerManager;
 import android.os.SystemClock;
 import android.provider.Settings.SettingNotFoundException;
@@ -14,9 +18,10 @@ import android.util.Log;
 //This one is the dismiss activity method but runs no guard activity
 //it just dismisses at wakeup and call end if the lockscreen is detected
 
-public class AutoDismiss extends MediatorService {
+public class AutoDismiss extends MediatorService implements SensorEventListener {
 	public boolean persistent = false;
     //public boolean timeoutenabled = false;
+	public boolean shakemode = false;
     
     public int patternsetting = 0;
     //we'll see if the user has pattern enabled when we startup
@@ -26,6 +31,56 @@ public class AutoDismiss extends MediatorService {
    
     //public boolean idle = false;
     //when the idle alarm intent comes in we set this true to properly start closing down
+    
+//============Shake detection variables
+    
+    private static final int FORCE_THRESHOLD = 350;
+    private static final int TIME_THRESHOLD = 100;
+    private static final int SHAKE_TIMEOUT = 500;
+    private static final int SHAKE_DURATION = 1000;
+    private static final int SHAKE_COUNT = 3;
+   
+    //private SensorManager mSensorMgr;
+    private float mLastX=-1.0f, mLastY=-1.0f, mLastZ=-1.0f;
+    private long mLastTime;
+    //private OnShakeListener mShakeListener;
+    private Context mContext;
+    private int mShakeCount = 0;
+    private long mLastShake;
+    private long mLastForce;
+    
+    //====
+    SensorManager mSensorEventManager;
+    
+    Sensor mSensor;
+    
+    
+    @Override
+    public void onCreate() {
+    	super.onCreate();
+    	
+    	//================register for shake listening, first time
+    	 SharedPreferences settings = getSharedPreferences("myLock", 0);
+         shakemode = settings.getBoolean("shake", false);
+    	
+        Log.v("init shake","registering for shake");
+        
+        mContext = getApplicationContext();
+        // Obtain a reference to system-wide sensor event manager.
+        mSensorEventManager = (SensorManager) mContext.getSystemService(Context.SENSOR_SERVICE);
+
+        // Get the default sensor for accel
+        mSensor = mSensorEventManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+
+        // Register for events.
+        if (shakemode) mSensorEventManager.registerListener(this, mSensor, SensorManager.SENSOR_DELAY_NORMAL);
+        //I don't have a use for shake while not in sleep (yet)
+        //workaround appears contingent on keeping it registered while awake also
+        //i will just unregister on first start
+        
+        //IntentFilter filter = new IntentFilter(Intent.ACTION_SCREEN_OFF);
+        //registerReceiver(mReceiver, filter);
+    }
     
     @Override
     public void onDestroy() {
@@ -56,13 +111,31 @@ public class AutoDismiss extends MediatorService {
                 
                 //ManageWakeLock.releasePartial();
                 
+             // Unregister from SensorManager.
+                if (shakemode) mSensorEventManager.unregisterListener(this);
+                //unregisterReceiver(mReceiver);
+                
 }
+    /*
+    private BroadcastReceiver mReceiver = new BroadcastReceiver() {
+        @Override
+ public void onReceive(Context context, Intent intent) {
+            // Check action just to be on the safe side.
+            if (intent.getAction().equals(Intent.ACTION_SCREEN_OFF)) {
+                // Unregisters the listener and registers it again.
+                mSensorEventManager.unregisterListener(AutoDismiss.this);
+                mSensorEventManager.registerListener(AutoDismiss.this, mSensor,
+                    SensorManager.SENSOR_DELAY_NORMAL);
+            }
+ }
+    };*/
 
     @Override
     public void onRestartCommand() {
             
             SharedPreferences settings = getSharedPreferences("myLock", 0);
             boolean fgpref = settings.getBoolean("FG", true);
+            boolean shakepref = settings.getBoolean("shake", false);
                                  
 /*========Settings change re-start commands that come from settings activity*/
     
@@ -72,10 +145,13 @@ public class AutoDismiss extends MediatorService {
                                     persistent = false;
                     }
                     else doFGstart();//so FG mode is started again
-            }       
+            }
+            else if (shakemode != shakepref) {
+            		shakemode = shakepref;
+            	}
             else {
 /*========Safety start that ensures the settings activity toggle button can work, first press to start, 2nd press to stop*/
-                            Log.v("toggle request","user first press of toggle after a startup at boot");
+                  Log.v("toggle request","user first press of toggle after a startup at boot");
                     }               
     }
     
@@ -88,6 +164,7 @@ public class AutoDismiss extends MediatorService {
             //timeoutenabled = settings.getBoolean("timeout", false);
                         
             if (persistent) doFGstart();
+            if (shakemode) mSensorEventManager.unregisterListener(this);
                         
             //we have to toggle pattern lock off to use a custom lockscreen
             try {
@@ -111,6 +188,7 @@ public class AutoDismiss extends MediatorService {
     			//editor.commit();
             }
             
+            
             //ManageWakeLock.acquirePartial(getApplicationContext());
             //if not always holding partial we would only acquire at Lock activity exit callback
             //we found we always need it to ensure key events will not occasionally drop on the floor from idle state wakeup
@@ -124,6 +202,9 @@ public class AutoDismiss extends MediatorService {
             
             IntentFilter lockStop = new IntentFilter ("i4nc4mp.myLock.lifecycle.LOCKSCREEN_EXITED");
             registerReceiver(lockStopped, lockStop);
+            
+         
+            
             
             editor.putBoolean("serviceactive", true);
             editor.commit();
@@ -156,6 +237,7 @@ public class AutoDismiss extends MediatorService {
         //it would ignore all user activity pokes and log "ignoring user activity while turning off screen"
         
         ManageWakeLock.releaseFull();
+        if (shakemode) mSensorEventManager.unregisterListener(AutoDismiss.this);
         return;
         }};
     
@@ -167,6 +249,15 @@ public class AutoDismiss extends MediatorService {
     	if (KG) StartDismiss(getApplicationContext());                
         
     	return;
+    }
+    
+    @Override
+    public void onScreenSleep() {
+    	//mSensorEventManager.unregisterListener(AutoDismiss.this);
+        if (shakemode) mSensorEventManager.registerListener(AutoDismiss.this, mSensor,
+            SensorManager.SENSOR_DELAY_NORMAL);
+        //standard workaround runs the listener at all times.
+        //i will only register at off and release it once we are awake
     }
     
     public void StartDismiss(Context context) {
@@ -192,12 +283,6 @@ public class AutoDismiss extends MediatorService {
     //my testing shows it actually comes back after any timeout sleep plus 5 sec grace period
     //then phone is doing a KM disable command at re-wake. and restoring at call end
     //that restore is what we intercept in these events as well as certain treatment based on lock activity lifecycle
-    
-    @Override
-    public void onCallStart() {
-            
-            Log.v("mediator - call start","call was placed or answered.");
-    }
     
     @Override
     public void onCallEnd() {
@@ -255,6 +340,51 @@ public class AutoDismiss extends MediatorService {
     }
     
 //============================
+    
+    public void onShake() {
+   	
+   	Log.v("onShake","doing wakeup");
+   	StartDismiss(getApplicationContext());
+   	//this causes wake as it happens just due to our code that's there already to prevent invalid sleep
+       
+    }
+    
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+        //not used right now
+    }
+    
+    //Used to decide if it is a shake
+    public void onSensorChanged(SensorEvent event) {
+            if(event.sensor.getType() != Sensor.TYPE_ACCELEROMETER) return;
+            
+            Log.v("sensor","sensor change is verifying");
+        long now = System.currentTimeMillis();
+     
+        if ((now - mLastForce) > SHAKE_TIMEOUT) {
+          mShakeCount = 0;
+        }
+     
+        if ((now - mLastTime) > TIME_THRESHOLD) {
+          long diff = now - mLastTime;
+          float speed = Math.abs(event.values[SensorManager.DATA_X] + event.values[SensorManager.DATA_Y] + event.values[SensorManager.DATA_Z] - mLastX - mLastY - mLastZ) / diff * 10000;
+          if (speed > FORCE_THRESHOLD) {
+            if ((++mShakeCount >= SHAKE_COUNT) && (now - mLastShake > SHAKE_DURATION)) {
+              mLastShake = now;
+              mShakeCount = 0;
+              
+            //call the reaction you want to have happen
+              onShake();
+            }
+            mLastForce = now;
+          }
+          mLastTime = now;
+          mLastX = event.values[SensorManager.DATA_X];
+          mLastY = event.values[SensorManager.DATA_Y];
+          mLastZ = event.values[SensorManager.DATA_Z];
+        }
+            
+    }
+//====End shake handling block
     
     void doFGstart() {
             //putting ongoing notif together for start foreground
