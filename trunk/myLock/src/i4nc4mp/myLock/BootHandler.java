c6c29908e100d4ccc7a8208f247aa4f963b577cf
myLockandroid <myLockandroid@d2e66b82-e105-11de-81d3-db62269da9c1>
2010-04-14 00:19:48 +0000

    Major update to the boot startup logic. New process handles user unlock of the non-secure and starts using phone after the reboot. We launch activity that sends the screen to sleep, which user would react to by hitting power. First press actually sends device to sleep (closing the lockdown screen as the secure lockscreen opens) then the 2nd press wakes up to the security verification. There is a bug in which the home key app exit causes activity start requests to get stopped and put on hold, so I can't reliably re-launch the screen when home is used to exit it. As a result we send to sleep almost instantly. If user can get out of the lockdown screen via home as soon as it appears, before the sleep happens, then they may be able to uninstall the app but they still won't be able to disable security since they don't know the pattern. But, since secure is active, the home button actually becomes disabled once the screen is asleep, thanks to the power manager. We don't have another good way to disable it though besides manually forcing the activity to be the user's default home. I've seen that workaround in the Lock 2.0 app.

--- trunk/myLock/src/i4nc4mp/myLock/BootHandler.java
+++ trunk/myLock/src/i4nc4mp/myLock/BootHandler.java
@@ -4,6 +4,7 @@ import android.app.Service;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.IBinder;
+import android.provider.Settings.SettingNotFoundException;
 import android.util.Log;
 
 public class BootHandler extends Service {
@@ -28,6 +29,8 @@ public class BootHandler extends Service {
 
 		boolean active = settings.getBoolean("serviceactive", false);
 		
+		boolean waitforuser = true;
+		
 		if (active) {
 			editor.putBoolean("serviceactive", false);
 		
@@ -35,25 +38,48 @@ public class BootHandler extends Service {
 			editor.commit();
 		}
 		
+		Intent u = new Intent();
+		u.setClassName("i4nc4mp.myLock", "i4nc4mp.myLock.UserPresentService");
+		
 		Intent i = new Intent();
+		i.setClassName("i4nc4mp.myLock", "i4nc4mp.myLock.Toggler");
+		i.putExtra("i4nc4mp.myLock.TargetState", true);
 		
 		//security mode requires that we force security on at boot and launch user present
-		//we basically treat boot like an idle timeout
-		if (secure) {
-			//ManageKeyguard.disableKeyguard(getApplicationContext());
-			
-			android.provider.Settings.System.putInt(getContentResolver(), 
-                    android.provider.Settings.System.LOCK_PATTERN_ENABLED, 1);
-			
-			i.setClassName("i4nc4mp.myLock", "i4nc4mp.myLock.UserPresentService");		
-			
-		}
-		else {
-			i.setClassName("i4nc4mp.myLock", "i4nc4mp.myLock.Toggler");
-  			i.putExtra("i4nc4mp.myLock.TargetState", true);
-		}
+		//so, only when system's security is off, we launch user present
+		//it will handle if user unlocked the non-secure KG immediately at startup
 		
-		startService(i);//start user present or toggler
+		if (secure) {
+			
+			int patternsetting = 0;
+
+	        try {
+	                patternsetting = android.provider.Settings.System.getInt(getContentResolver(), android.provider.Settings.System.LOCK_PATTERN_ENABLED);
+	        } catch (SettingNotFoundException e) {
+	                // TODO Auto-generated catch block
+	                e.printStackTrace();
+	        }
+	        
+	        boolean s = (patternsetting == 1);
+			
+			if (!s) {
+				android.provider.Settings.System.putInt(getContentResolver(), 
+						android.provider.Settings.System.LOCK_PATTERN_ENABLED, 1);
+			}
+			else {
+				ManageKeyguard.initialize(getApplicationContext());
+				if (!ManageKeyguard.inKeyguardRestrictedInputMode()) waitforuser = false;
+					
+				//security was already on and phone has been unlocked
+				//so no need to wait
+				}
+		}
+		else waitforuser = false;
+		
+		//Don't wait if not aware of security or we already started system secure and got are already unlocked
+		
+		if (waitforuser) startService(u);//start user present
+		else startService(i);//start toggler
 						
 		stopSelf();
 		
