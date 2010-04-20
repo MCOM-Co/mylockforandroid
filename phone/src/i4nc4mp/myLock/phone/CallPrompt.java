abf805d11d20788e9e540841624f6badec4cdb93
myLockandroid <myLockandroid@d2e66b82-e105-11de-81d3-db62269da9c1>
2010-04-20 04:47:25 +0000

    Commit alpha code for the accept call activity we put up during ringing call. we will evolve this to be customizable. this app will also include the ability to have a touchscreen guard up during calls. the guard just blocks the screen from receiving touch events but is easy to dismiss

---
+++ phone/src/i4nc4mp/myLock/phone/CallPrompt.java
@@ -0,0 +1,65 @@
+package i4nc4mp.myLock.phone;
+
+import android.app.Activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+public class CallPrompt extends Activity {
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+				
+		
+		setContentView(R.layout.main);
+			
+		
+		Button answer = (Button) findViewById(R.id.mid);
+		
+		answer.setOnClickListener(new OnClickListener() {
+          	public void onClick(View v){
+          		answer();
+          	}
+		});
+		
+		//I would actually like to have the power key reject
+		//can't figure out if we can "cause" reject
+		
+	}
+	
+	
+	void answer() {
+		Intent new_intent = new Intent(Intent.ACTION_MEDIA_BUTTON);
+
+  		//most certainly does work
+		//special thanks the auto answer open source app
+		//which demonstrated this answering functionality
+  		new_intent.putExtra(Intent.EXTRA_KEY_EVENT, new KeyEvent(KeyEvent.ACTION_DOWN, KeyEvent.KEYCODE_HEADSETHOOK));
+  		sendOrderedBroadcast(new_intent, null);
+  		moveTaskToBack(true);
+  		finish();
+	}
+	
+	//let's allow the camera press to accept this call
+	@Override
+    public boolean dispatchKeyEvent(KeyEvent event) {
+		switch (event.getKeyCode()) {
+		case KeyEvent.KEYCODE_FOCUS:
+			return true;
+			//this event occurs - if passed on, phone retakes focus
+			//so let's consume it to avoid that outcome
+		case KeyEvent.KEYCODE_CAMERA:
+			answer();
+			return true;
+		default:
+			break;
+		}
+		return super.dispatchKeyEvent(event);
+	}
+}
