0a09fdd1a2096c75632fe4f01d5ce0a0ad86def7
myLockandroid <myLockandroid@d2e66b82-e105-11de-81d3-db62269da9c1>
2010-04-22 04:58:34 +0000

    Very small tweak to make the pref screen reference the existing file we declared. this way we aren't juggling the default pref activity created file plus the old myLock one.

--- myLockcupcake/src/i4nc4mp/myLock/cupcake/MainPreferenceActivity.java
+++ myLockcupcake/src/i4nc4mp/myLock/cupcake/MainPreferenceActivity.java
@@ -8,5 +8,8 @@ public class MainPreferenceActivity extends PreferenceActivity {
         protected void onCreate(Bundle savedInstanceState) {
                 super.onCreate(savedInstanceState);
                 addPreferencesFromResource(R.xml.preferences);
+                getPreferenceManager().setSharedPreferencesName("myLock");
+                //tell the prefs persisted to go in the already existing pref file
+                //this way we aren't juggling our old file & the "default" normally used by pref activity
         }
 }
