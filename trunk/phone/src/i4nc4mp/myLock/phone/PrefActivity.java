f2bc03c2815a854337f103d9c0a140c6a635c823
myLockandroid <myLockandroid@d2e66b82-e105-11de-81d3-db62269da9c1>
2010-04-28 04:32:17 +0000

    Just about finished. prefs implemented, need to put in the basic icon. works as expected in emulator

---
+++ trunk/phone/src/i4nc4mp/myLock/phone/PrefActivity.java
@@ -0,0 +1,34 @@
+package i4nc4mp.myLock.phone;
+
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.preference.CheckBoxPreference;
+import android.preference.PreferenceActivity;
+
+public class PrefActivity extends PreferenceActivity {
+	
+	@Override
+    protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+                            
+            addPreferencesFromResource(R.xml.pref);
+            
+            getPreferenceManager().setSharedPreferencesName("myLockphone");
+	}
+	
+	@Override
+    protected void onStart() {
+    	super.onStart();
+    	
+    	getPrefs();
+    }
+    
+    private void getPrefs() {
+    	//we need to show the user's existing prefs, this isn't done automatically by the activity
+    	SharedPreferences myprefs = getSharedPreferences("myLockphone", 0);
+    	
+    	((CheckBoxPreference) findPreference("callPrompt")).setChecked(myprefs.getBoolean("callPrompt", false));
+    	((CheckBoxPreference) findPreference("cameraAccept")).setChecked(myprefs.getBoolean("cameraAccept", false));
+    	((CheckBoxPreference) findPreference("touchLock")).setChecked(myprefs.getBoolean("touchLock", false));
+}
+}
